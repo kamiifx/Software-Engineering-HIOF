55fe66b0af5567d824dd80bec7d7cfb4ca91561b
Kamii Akbari <ungewang@gmail.com>
2020-10-12 15:55:32 +0200

    Tests Finish

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -5,10 +6,15 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeapYearTest {
+    LeapYear ly;
+
+    @BeforeEach
+    public void setup(){
+        ly = new LeapYear();
+    }
 
     @Test
-    public void testLeapYearTrue(){
-        LeapYear ly = new LeapYear();
+    public void testLeapYearTrue(){             //Test for skuddår
         assertTrue(ly.isLeapYear(2000));
         assertTrue(ly.isLeapYear(2020));
         assertTrue(ly.isLeapYear(2024));
@@ -17,13 +23,16 @@ class LeapYearTest {
     }
 
     @Test
-    public void testLeapYearFalse(){
-        LeapYear ly = new LeapYear();
+    public void testLeapYearFalse(){            //Test for ikke skuddår
         assertFalse(ly.isLeapYear(1700));
         assertFalse(ly.isLeapYear(1800));
         assertFalse(ly.isLeapYear(1900));
         assertFalse(ly.isLeapYear(2100));
     }
+
+
+
+    // Bonus Oppgaver
 
     @ParameterizedTest
     @ValueSource(ints = { 1896, 1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956,
@@ -31,7 +40,6 @@ class LeapYearTest {
             2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088, 2092, 2096, 2104, 2108, 2112, 2116, 2120, 2124, 2128, 2132, 2136, 2140, 2144, 2148,
             2152, 2156, 2160, 2164, 2168, 2172, 2176, 2180, 2184, 2188, 2192, 2196})
     public void testLeapYearAllTrue(int x){
-        LeapYear ly = new LeapYear();
         assertTrue(ly.isLeapYear(x));
     }
 
@@ -40,7 +48,6 @@ class LeapYearTest {
             2057, 2061, 2065, 2069, 2073, 2077, 2081, 2085, 2089, 2093, 2097, 2105, 2109, 2113, 2117, 2121, 2125, 2129, 2133, 2137, 2141, 2145, 2149,
             2153, 2157, 2161, 2165, 2169, 2173, 2177, 2181, 2185, 2189, 2193, 2197})
     public void testLeapYearAllFalse(int x){
-        LeapYear ly = new LeapYear();
         assertFalse(ly.isLeapYear(x));
     }
 }
